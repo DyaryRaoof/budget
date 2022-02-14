4c4c77b9d0441f51c43f63f41bf316fba1face6d
DyaryRaoof <dyary2020@gmail.com>
2022-02-14 19:55:33 +0300

    add name field to devise and fix redirect error on registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'home#index'
+  resources :users do
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
