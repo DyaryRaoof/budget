7cce240d51b69cad684ad77a92e5210faad8ca45
DyaryRaoof <dyary2020@gmail.com>
2022-02-14 20:49:45 +0300

    Add validation to spendings model

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -1,3 +1,6 @@
 class Spending < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
