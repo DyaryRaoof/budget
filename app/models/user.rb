6acf598fda1d45d9f0bd7c352a392cc6062da354
DyaryRaoof <dyary2020@gmail.com>
2022-02-14 21:46:01 +0300

    Add seeds and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :groups, dependent: :destroy
+  has_many :spendings, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
