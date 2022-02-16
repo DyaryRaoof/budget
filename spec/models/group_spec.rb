9e91efdaa2a94765e56ab3fac83ec448293974ed
DyaryRaoof <dyary2020@gmail.com>
2022-02-16 20:54:47 +0300

    Add model specs and request specs for groups

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Group.new(name: 'name', icon: 'group icon', author_id: User.first.id) }
+  before { subject.save }
+
+  it 'name should be name' do
+    expect(subject.name).to eq 'name'
+  end
+
+  it 'object should be invalid' do
+    subject.name =  nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'object should be invalid' do
+    subject.icon =  nil
+    expect(subject).to_not be_valid
+  end
+
 end
--- spec/models/spending_spec.rb
+++ spec/models/spending_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Spending, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Spending.new(name: 'name', amount: 11) }
+  before { subject.save }
+
+  it 'name should be name' do
+    expect(subject.name).to eq 'name'
+  end
+
+  it 'object should be invalid' do
+    subject.name =  nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'object should be invalid' do
+    subject.amount =  nil
+    expect(subject).to_not be_valid
+  end
 end
