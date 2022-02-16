a86d981d28c964fdf6e26e3410fa9d1ad405aca7
DyaryRaoof <dyary2020@gmail.com>
2022-02-16 23:04:56 +0300

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    if user = current_user
-      can :manage, :all
-    end
+    can :manage, :all if user == current_user
 
     # The first argument to `can` is the action you are giving the user
     # permission to do.
