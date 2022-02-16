6d565d7fc122f44714cd3dc631b7443a86a5dbb6
DyaryRaoof <dyary2020@gmail.com>
2022-02-16 23:02:47 +0300

    Change cancancan authentication

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,8 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user = current_user
       can :manage, :all
-    else
-      can :read, :all
     end
 
     # The first argument to `can` is the action you are giving the user
